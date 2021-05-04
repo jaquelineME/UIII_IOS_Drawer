e15027539fdce32cc6223d97276f5bcecb839529
FlutLab.io <>
2021-05-04 19:47:50 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MelendezApp());
+
+class MelendezApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ), //Fin de theme
+      home: EsScaffold(title: 'Flutter en Español Meléndez'),
+    ); // fin de materialApp
+  } // fin de widget
+} // fin de MelendezApp
+
+class EsScaffold extends StatefulWidget {
+  EsScaffold({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _EsScaffoldState createState() => _EsScaffoldState();
+} // fin de EsScaffold
+
+class _EsScaffoldState extends State<EsScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'assets/FlutterEnEspanol.jpg',
+          width: 200.0,
+        ), //Fin image
+      ), //fin de center
+      drawer: Drawer(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          FlutterLogo(size: 100.0),
+          Text('Drawer 1 por Meléndez'),
+        ]), //Fin de columna
+      ), //Fin drawer1
+      endDrawer: Drawer(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          FlutterLogo(size: 100.0),
+          Text('Drawer 2 por Meléndez'),
+        ]), //Fin de columna
+      ), //Fin drawer2
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.favorite),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                height: 200.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FlutterLogo(size: 100.0),
+                    Text('Bottom sheet Meléndez'),
+                  ],
+                ),
+              ); //Fin Container
+            }, //Fin builder
+          ); //Fin Sheet
+        }, //Fin de onPressed
+      ), //Fin floatingActionButton
+
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.pink,
+        child: Container(
+          height: 50.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FlutterLogo(
+                size: 30.0,
+              ),
+              Text('bottomNavigationBar Jaqueline Meléndez')
+            ],
+          ),
+        ),
+      ), //Fin de bottomAppBar
+    ); // fin de Saffold
+  } //fin de widget construir
+} // fin de state<EsScaffold>
